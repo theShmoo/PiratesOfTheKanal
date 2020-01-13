1c87bec9d66f25ef6f737e3046ce64638626ffe5
David Pfahler <pfahler.david@gmail.com>
2020-01-14 00:20:03 +0100

    added builded web page

---
+++ build/precache-manifest.d593a57f64d3974e9ced3be88e88d26d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7600798b66dfc55c78a7febdc952c8f2",
+    "url": "/PiratesOfTheKanal/index.html"
+  },
+  {
+    "revision": "9e12b883fb67dd406f66",
+    "url": "/PiratesOfTheKanal/static/js/2.fac3789b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/PiratesOfTheKanal/static/js/2.fac3789b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0b1e1e2b34c738f7058e",
+    "url": "/PiratesOfTheKanal/static/js/main.277cc961.chunk.js"
+  },
+  {
+    "revision": "38eb4d53ac5d640dc2bc",
+    "url": "/PiratesOfTheKanal/static/js/runtime-main.d2477cb1.js"
+  }
+]);
