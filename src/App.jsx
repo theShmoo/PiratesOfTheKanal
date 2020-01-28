5f471f04f2c387ff9751be16d45f15f3fd91358c
David Pfahler <pfahler@vrvis.at>
2020-01-28 16:14:29 +0100

    changed to other landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,65 +1,95 @@
 import React from 'react';
-import { Parallax } from 'react-parallax';
-import {useWindowDimensions} from './WindowDimensions'
+import {
+  Provider, Heading, Subhead, Flex
+} from 'rebass'
+import {
+  Hero, ScrollDownIndicator, Section, Checklist, Testimony
+} from 'react-landing-page'
 
-const styles = {
-  fontFamily: "sans-serif",
-  textAlign: "center",
-  backgroundColor: "#e8eaf6"
-};
-const insideStyles = {
-  position: "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%,-50%)",
-  color: "black",
-  fontSize: 40,
-  fontWeight: 100,
-  width: "300px",
-  backgroundColor: "rgba(255,255,255,0.8)",
-  padding: 30,
-  borderRadius: 10,
-};
-// const Separator = () => { return <h1>| | |</h1>}
-const ParallaxBox = (props) => {
-  const { height } = useWindowDimensions();
+const featherCheckmark = <svg
+  xmlns="http://www.w3.org/2000/svg"
+  width="24" height="24"
+  viewBox="0 0 24 24"
+  fill="none" stroke="currentColor"
+  strokeWidth="2"
+  strokeLinecap="round"
+  strokeLinejoin="round"
+>
+  <path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"/>
+  <polyline points="22 4 12 14.01 9 11.01"/>
+</svg>
 
-  return <div style={{
-    height: height,
-  }}>
-    <div style={insideStyles}>{props.children}</div>
-  </div>
-}
-const MyParallax = (props) => {
-  const { height } = useWindowDimensions();
-
-  return <div>
-    <Parallax
-      bgImage={props.img}
-      strength={300}
-      bgImageStyle={{
-        width: "auto",
-        height: height,
-      }}
-      blur={{ min: -1, max: 3 }}
+const FastHero = (props) => {
+    return <Hero
+      color="black"
+      bg="white"
+      bgOpacity={props.opacity ? props.opacity : 0.4}
+      backgroundImage={props.img}
     >
-      <ParallaxBox>
-        {props.children}
-      </ParallaxBox>
-    </Parallax>
-  </div>
+      {props.heading ? <Heading fontSize={[ 4, 5, 6, 7, 8 ]}>{props.heading}</Heading> : ""}
+      {props.subheading ? <Subhead fontSize={[ 1, 2, 3, 4, 5 ]}>{props.subheading}</Subhead> : ""}
+      <ScrollDownIndicator/>
+    </Hero>
 }
 
-const App = () => (
-  <div style={styles}>
-    <MyParallax img={'img/1.jpg'}>Pirates of the Kanal</MyParallax>
-    <MyParallax img={'img/8.jpg'}>das Badner Tubing Event</MyParallax>
-    <MyParallax img={'img/9.jpg'}>am Wiener Neustädter Kanal</MyParallax>
-    <MyParallax img={'img/2.jpg'}>22. August 2020</MyParallax>
-    <MyParallax img={'img/5.jpg'}>15:00 Uhr</MyParallax>
-    <MyParallax img={'img/3.jpg'}>bis Sonnenuntergang</MyParallax>
-    <MyParallax img={'img/6.jpg'}>mit vielen Features</MyParallax>
-    <MyParallax img={'img/7.jpg'} last={true}>Keep calm and float on</MyParallax>
-  </div>
-);
+const App = props => (
+  <Provider>
+    <FastHero
+      img="img/1.jpg"
+      heading="Pirates of the Kanal"
+      subheading="das Badner Tubing Event" />
+    <Section width={1}
+      heading='Hard Facts'>
+      <Checklist
+        children={[
+          'auf dem Wiener Neustädter Kanal',
+          'am 22. August 2020',
+          'von 15:00 Uhr',
+          'bis Sonnenuntergang',
+        ]}
+        checkmark={featherCheckmark}/>
+    </Section>
+    <FastHero
+      img="img/7.jpg"
+      heading="Keep calm and float on"
+      subheading="Relax, Recover, Restore"/>
+    <Section
+      width={1}
+      heading='Was unsere Tuber sagen'
+      justifyContent="space-around">
+        <Testimony
+          authorAvatar="img/avatar_2.jpg"
+          authorTitle="Tuber">
+             Amazing Experience and I would definitely do it again. Good services, staff were very friendly and inviting. A life time experience.
+        </Testimony>
+        <Testimony
+          authorAvatar="img/avatar.jpg"
+          authorName="Lakob Julich"
+          authorTitle="Tubing Expert">
+            Best. Tubing. Ever.
+        </Testimony>
+    </Section>
+    <FastHero
+      img="img/4.jpg"
+      heading="Stay Tubed!"/>
+    <Section width={1}
+      heading='Du brauchst mit'>
+      <Checklist
+        children={[
+          'Einen Tube',
+          'Proviant',
+          'Zuschauer',
+        ]}
+        checkmark={featherCheckmark}/>
+    </Section>
+    <FastHero
+      img="img/8.jpg"
+      opacity={0.01}
+    />
+    <Section width={1}
+      heading='Tube on'>
+    </Section>
+  </Provider>
+)
+
 export default App;
