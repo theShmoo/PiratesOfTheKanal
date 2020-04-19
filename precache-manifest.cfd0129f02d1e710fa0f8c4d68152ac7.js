5c35e1c9febf5ba57c5c35e5f4614b5f443e832c
David Pfahler <pfahler.david@gmail.com>
2020-04-19 19:53:54 +0200

    Updates

---
+++ precache-manifest.cfd0129f02d1e710fa0f8c4d68152ac7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67077f61fd6ddbccc11a3f8756ad22a1",
+    "url": "/PiratesOfTheKanal/index.html"
+  },
+  {
+    "revision": "921a6fde829fe9d8b6cf",
+    "url": "/PiratesOfTheKanal/static/css/2.6f3a26c5.chunk.css"
+  },
+  {
+    "revision": "921a6fde829fe9d8b6cf",
+    "url": "/PiratesOfTheKanal/static/js/2.7c2fc7c2.chunk.js"
+  },
+  {
+    "revision": "278be41389e3536a5347a9018737729d",
+    "url": "/PiratesOfTheKanal/static/js/2.7c2fc7c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c55c96769d2512aba3ed",
+    "url": "/PiratesOfTheKanal/static/js/main.63db5aee.chunk.js"
+  },
+  {
+    "revision": "90777c152055b864b751",
+    "url": "/PiratesOfTheKanal/static/js/runtime-main.9207724e.js"
+  }
+]);
