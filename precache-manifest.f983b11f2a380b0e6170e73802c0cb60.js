5392adb2945dec0b4c029550baa87377a391ae5c
David Pfahler <pfahler.david@gmail.com>
2020-04-28 17:20:06 +0200

    Updates

---
+++ precache-manifest.f983b11f2a380b0e6170e73802c0cb60.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b092e96f7b14e42e26bdd7dbf77463aa",
+    "url": "./index.html"
+  },
+  {
+    "revision": "921a6fde829fe9d8b6cf",
+    "url": "./static/css/2.6f3a26c5.chunk.css"
+  },
+  {
+    "revision": "921a6fde829fe9d8b6cf",
+    "url": "./static/js/2.7c2fc7c2.chunk.js"
+  },
+  {
+    "revision": "278be41389e3536a5347a9018737729d",
+    "url": "./static/js/2.7c2fc7c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34493d81f8a36cd0002b",
+    "url": "./static/js/main.2d57e740.chunk.js"
+  },
+  {
+    "revision": "7cf3f341e42d41ec2e37",
+    "url": "./static/js/runtime-main.5e270387.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/PiratesOfTheKanal/precache-manifest.b6c1ebbe876b8f7e9835f37a0acc39b3.js"
+  "./precache-manifest.f983b11f2a380b0e6170e73802c0cb60.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/PiratesOfTheKanal/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
