fb24ed173419cb7fb9cc0b91b6c355f6faca3ffe
David Pfahler <pfahler@vrvis.at>
2020-08-17 08:57:35 +0200

    Updates

---
+++ precache-manifest.b4858ee04b0ac864fa483b7a4dc55198.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5684859edaac3cb9f3731177f9a8edac",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ee456bcdbdd783bc3952",
+    "url": "./static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "ee456bcdbdd783bc3952",
+    "url": "./static/js/2.6e4f2db3.chunk.js"
+  },
+  {
+    "revision": "278be41389e3536a5347a9018737729d",
+    "url": "./static/js/2.6e4f2db3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04f6fa8ee2ec83c24a48",
+    "url": "./static/js/main.391ae024.chunk.js"
+  },
+  {
+    "revision": "c54d8060641ce0b95907",
+    "url": "./static/js/runtime-main.2e0acd72.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.519e11b1e8359545cce26392ef361176.js"
+  "./precache-manifest.b4858ee04b0ac864fa483b7a4dc55198.js"
 );
 
 self.addEventListener('message', (event) => {
