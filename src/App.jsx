6ab82ee89a433d28f67ca901072e61804ff43735
David Pfahler <pfahler@vrvis.at>
2020-08-20 13:51:16 +0200

    updated some infotext and the packages

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,6 @@ const useStyles = makeStyles((theme: Theme) =>
       height: '100%',
     },
     list: {
-      maxWidth: 360
     },
     section: {
       margin: theme.spacing(3),
@@ -52,14 +51,14 @@ const useStyles = makeStyles((theme: Theme) =>
   }),
 );
 
-const Heading = ({children}) => {
+const Heading = ({ children }) => {
   const classes = useStyles();
   return <Typography variant="h4" color="primary" className={classes.text}>
     {children}
   </Typography>
 };
 
-const Subheading = ({children}) => {
+const Subheading = ({ children }) => {
   const classes = useStyles();
   return <Typography variant="h5" color="secondary" className={classes.text}>
     {children}
@@ -67,60 +66,60 @@ const Subheading = ({children}) => {
 }
 
 
-const Section = ({heading, subheading, children}) => {
+const Section = ({ heading, subheading, children }) => {
   const classes = useStyles();
   return <Paper square elevation={4}>
     <Grid item xs={12} className={classes.section}>
-    {heading ? <Heading>{heading}</Heading> : ""}
-    {subheading ? <Subheading>{subheading}</Subheading> : ""}
-    {children}
-  </Grid>
+      {heading ? <Heading>{heading}</Heading> : ""}
+      {subheading ? <Subheading>{subheading}</Subheading> : ""}
+      {children}
+    </Grid>
   </Paper>
 }
 
-const Checklist = ({items, checkmark}) => {
+const Checklist = ({ items, checkmark }) => {
   const classes = useStyles();
   const listItems = items.map((item, i) => <ListItem key={i} button>
-      <ListItemIcon>
-        <CheckCircleIcon />
-      </ListItemIcon>
-      <ListItemText primary={item} />
-    </ListItem>
+    <ListItemIcon>
+      <CheckCircleIcon />
+    </ListItemIcon>
+    <ListItemText primary={item} />
+  </ListItem>
   );
   return <List component="nav" className={classes.list}>
     {listItems}
-    </List>
+  </List>
 }
 
-const Hero = ({heading, subheading, img, children}) => {
+const Hero = ({ heading, subheading, img, children }) => {
   const classes = useStyles();
   const style = img ? {
-        background: "url(" + img + ") no-repeat center center",
-        backgroundSize: "cover",
-        resize: "both",
-      } : {};
+    background: "url(" + img + ") no-repeat center center",
+    backgroundSize: "cover",
+    resize: "both",
+  } : {};
   return <Paper square elevation={4} className={classes.hero}>
-      <Grid item xs={12}
-        className={classes.fullscale}
-        style={style}
-      >
-        {(heading || subheading) ? <Box bgcolor="rgba(255, 255, 255, .9)" className={classes.opaque}>
-          {heading ? <Heading>{heading}</Heading> : ""}
-          {subheading ? <Subheading>{subheading}</Subheading> : ""}
-        </Box> : ""}
-        {children}
-      </Grid>
-    </Paper>
+    <Grid item xs={12}
+      className={classes.fullscale}
+      style={style}
+    >
+      {(heading || subheading) ? <Box bgcolor="rgba(255, 255, 255, .9)" className={classes.opaque}>
+        {heading ? <Heading>{heading}</Heading> : ""}
+        {subheading ? <Subheading>{subheading}</Subheading> : ""}
+      </Box> : ""}
+      {children}
+    </Grid>
+  </Paper>
 }
 
-const Testimony = ({avatar, name, title, children}) => {
+const Testimony = ({ avatar, name, title, children }) => {
   return <ListItem alignItems="flex-start">
-  <ListItemAvatar>
-    <Avatar src={avatar} />
-  </ListItemAvatar>
-  <ListItemText
-    primary={name}
-    secondary={children} />
+    <ListItemAvatar>
+      <Avatar src={avatar} />
+    </ListItemAvatar>
+    <ListItemText
+      primary={name}
+      secondary={children} />
   </ListItem>
 }
 
@@ -144,12 +143,36 @@ const App = props => {
           "am 22. August 2020",
           "ab 15:00 Uhr",
           "bis Sonnenuntergang",
-        ]}/>
+        ]} />
+    </Section>
+    <Hero
+      img="img/9.jpg" />
+    <Section heading="Floating Facts">
+      <Typography variant="body1">
+        Wir bitten um eine <a href="#register">Anmeldung</a>, damit wir die Teilnehmerzahl abschätzen können 👍
+        </Typography>
+      <Typography variant="body1">
+        Außerdem empfehlen wir:
+        </Typography>
+      <Checklist
+        items={[
+          "Tragen eines MNS Maske am Treffpunkt und beim Ausstieg",
+          "Einhalten des Mindesabstandes",
+        ]} />
+      <Typography variant="body1">
+        Noch ein paar Infos:
+        </Typography>
+      <Checklist
+        items={[
+          "Die Aktion findet nur bei Schönwetter statt (eventuelle Absage erfolgt am Freitag)",
+          "Es wird eine Pumpe zum Aufblasen am Treffpunkt vorhanden sein.",
+          "Alternativ kann am Samstag zwischen 12 und 13:30 das Tube in der Meiereigasse 12, 2500 Baden abgegeben werden und es wird aufgeblasen zum Treffpunkt transportiert werden"
+        ]} />
     </Section>
     <Hero
       img="img/7.jpg"
       heading="Keep calm and float on"
-      subheading="Relax, Recover, Restore"/>
+      subheading="Relax, Recover, Restore" />
     <Section
       heading="Was unsere Tuber sagen">
       <List component="nav" className={classes.list}>
@@ -157,26 +180,28 @@ const App = props => {
           avatar="img/avatar_2.jpg"
           name="Q"
           title="Tuber">
-             Amazing Experience and I would definitely do it again. Good services, staff were very friendly and inviting. A life time experience.
+          Amazing Experience and I would definitely do it again. Good services, staff were very friendly and inviting. A life time experience.
         </Testimony>
         <Testimony
           avatar="img/avatar.jpg"
           name="Lakob"
           title="Tubing Expert">
-            Best. Tubing. Ever.
+          Best. Tubing. Ever.
         </Testimony>
       </List>
     </Section>
     <Hero
       img="img/5.jpg"
-      heading="Aber Wo genau?"/>
+      heading="Aber Wo genau?" />
     <Section heading="Da." />
     <Hero>
       <MyMap />
     </Hero>
-    <Section heading="Stay Tubed!">
-      <FormDialog />
-    </Section>
+    <div id="register">
+      <Section heading="Stay Tubed!">
+        <FormDialog />
+      </Section>
+    </div>
     <Hero
       img="img/4.jpg">
     </Hero>
@@ -186,7 +211,7 @@ const App = props => {
           "Tube",
           "Proviant",
           "Zuschauer",
-        ]}/>
+        ]} />
     </Section>
     <Hero
       img="img/8.jpg"
